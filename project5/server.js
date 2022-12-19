3c69bab7282a096c13e71057f73f6f7e2dcbe614
TRUP03 <truptee20_ug@ee.nits.ac.in>
2022-12-19 23:35:13 +0530

    1

---
+++ project5/server.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('login vth password');
+});
+
+app.listen(3000,()=>{
+    console.log('Server running at port 3000');
+});
